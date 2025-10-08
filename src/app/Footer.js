edd8b7cdec77c60466da0fc94b057ab99df6a7c3
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-10-08 15:41:55 +0200

    Footer added

---
+++ src/app/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <div>
+      Created by Lilla,{" "}
+      <a
+        className="font-bold"
+        href="https://github.com/Lilla-ctrl/meal-planner.git"
+      >
+        open-source code
+      </a>{" "}
+      on Github.
+    </div>
+  );
+}
