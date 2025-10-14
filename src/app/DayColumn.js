f31598a8c10bdb64d41ef62051fce67632c4e742
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-10-14 13:20:55 +0200

    DayColumn mapping in PlannerGrid

--- src/app/DayColumn.js
+++ src/app/DayColumn.js
@@ -1,7 +1,8 @@
-export default function DayColumn() {
+export default function DayColumn(props) {
   return (
     <div className=" w-60 p-4 rounded-2xl flex flex-col m-3 gap-2">
-      <h1 className="p-3 rounded-xl text-2xl font-bold w-full text-center">Monday</h1>
+      <h1 className="p-3 rounded-xl text-2xl font-bold w-full text-center">{props.day}
+        </h1>
       <ul className="flex flex-wrap justify-center gap-2 w-full">
         <li className="bg-warm-beige border border-gray-300 p-3 w-full text-center rounded-xl">Breakfast</li>
         <li className="bg-warm-beige border border-gray-300 p-3 w-full text-center rounded-xl">Snack *optional*</li>
--- src/app/PlannerGrid.js
+++ src/app/PlannerGrid.js
@@ -1,15 +1,21 @@
 import DayColumn from "./DayColumn";
 
 export default function PlannerGrid() {
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
   return (
     <div className="flex overflow-x-auto px-6 pb-1 m-5 rounded-xl shadow-2xl bg-light-background">
-      <DayColumn />
-      <DayColumn />
-      <DayColumn />
-      <DayColumn />
-      <DayColumn />
-      <DayColumn />
-      <DayColumn />
+      {days.map((day) => (
+        <DayColumn key={day} day={day} />
+      ))}
     </div>
-  )
+  );
 }
