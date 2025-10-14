943d396ee1c85e7c7314a5b76f64d9ca3086d365
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-10-14 14:41:39 +0200

    Update Modal.js
    Cancel button added

--- src/app/Modal.js
+++ src/app/Modal.js
@@ -5,7 +5,9 @@ export default function Modal(props) {
         <h1 className="text-3xl mb-5">Add meal</h1>
         <div className="flex flex-col items-center">
           <form action="" className="flex flex-col items-center">
-            <label htmlFor="meal" className="text-xl">Meal name</label>
+            <label htmlFor="meal" className="text-xl">
+              Meal name
+            </label>
             <input
               type="text"
               name="meal"
@@ -13,8 +15,14 @@ export default function Modal(props) {
               required
               className="shadow-inner p-2 my-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-amber-400"
             />
-            <label htmlFor="mealtype" className="text-xl">Meal type</label>
-            <select name="mealtype" id="mealtype" className="border border-gray-300 w-full py-1 my-2 rounded-lg text-center">
+            <label htmlFor="mealtype" className="text-xl">
+              Meal type
+            </label>
+            <select
+              name="mealtype"
+              id="mealtype"
+              className="border border-gray-300 w-full py-1 my-2 rounded-lg text-center"
+            >
               <option value="Breakfast">Breakfast</option>
               <option value="Midday snack">Midday snack</option>
               <option value="Lunch">Lunch</option>
@@ -22,12 +30,20 @@ export default function Modal(props) {
               <option value="Dinner">Dinner</option>
             </select>
           </form>
-          <button
-            onClick={props.handleClose}
-            className="bg-warm-button hover:bg-warm-beige border-1 border-gray-300 px-5 py-2 mt-5 rounded-xl cursor-pointer"
-          >
-            Close
-          </button>
+          <div className="flex gap-4 w-full">
+            <button
+              onClick={props.handleClose}
+              className="flex-1 bg-warm-button hover:bg-warm-beige border-1 border-gray-300 px-5 py-2 mt-5 rounded-xl cursor-pointer"
+            >
+              Save
+            </button>
+            <button
+              onClick={props.handleClose}
+              className="flex-1 bg-warm-button hover:bg-warm-beige border-1 border-gray-300 px-5 py-2 mt-5 rounded-xl cursor-pointer"
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     </div>
