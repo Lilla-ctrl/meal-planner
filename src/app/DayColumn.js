467e45cdb257be97b9494d3c0a7e509ae1787bb2
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-10-14 11:26:21 +0200

    Initial design of DayColumn

--- src/app/DayColumn.js
+++ src/app/DayColumn.js
@@ -1,8 +1,8 @@
 export default function DayColumn() {
   return (
     <div className="bg-light-cream w-60 p-4 rounded-2xl flex flex-col m-3 gap-2">
-      <h1 className="bg-warm-beige p-3 rounded-xl text-2xl w-full text-center">Monday</h1>
-      <ul className="flex flex-col gap-2 w-full">
+      <h1 className="bg-soft-orange p-3 rounded-xl text-2xl w-full text-center">Monday</h1>
+      <ul className="flex flex-wrap justify-center gap-2 w-full">
         <li className="bg-warm-beige p-3 w-full text-center rounded-xl">Breakfast</li>
         <li className="bg-warm-beige p-3 w-full text-center rounded-xl">Snack *optional*</li>
         <li className="bg-warm-beige p-3 w-full text-center rounded-xl">Lunch</li>
--- src/app/PlannerGrid.js
+++ src/app/PlannerGrid.js
@@ -2,7 +2,13 @@ import DayColumn from "./DayColumn";
 
 export default function PlannerGrid() {
   return (
-    <div>
+    <div className="flex overflow-x-auto p1-6 pb-1">
+      <DayColumn />
+      <DayColumn />
+      <DayColumn />
+      <DayColumn />
+      <DayColumn />
+      <DayColumn />
       <DayColumn />
     </div>
   )
