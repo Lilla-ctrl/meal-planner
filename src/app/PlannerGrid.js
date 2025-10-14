440614ca81230966e9cdf2a767fab272880f9bc3
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-10-14 13:54:38 +0200

    initial states created in PlannerGrid

--- src/app/PlannerGrid.js
+++ src/app/PlannerGrid.js
@@ -1,6 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import DayColumn from "./DayColumn";
 
 export default function PlannerGrid() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedDay, setSelectedDay] = useState("");
+  const [mealForDay, setMealForDay] = useState({
+    Monday: [],
+    Tuesday: [],
+    Wednesday: [],
+    Thursday: [],
+    Friday: [],
+    Saturday: [],
+    Sunday: [],
+  });
+
   const days = [
     "Monday",
     "Tuesday",
@@ -11,10 +26,14 @@ export default function PlannerGrid() {
     "Sunday",
   ];
 
+  function onClick() {
+    setModalOpen(true);
+  }
+
   return (
     <div className="flex overflow-x-auto px-6 pb-1 m-5 rounded-xl shadow-2xl bg-light-background">
       {days.map((day) => (
-        <DayColumn key={day} day={day} />
+        <DayColumn key={day} day={day} onClick={onClick} />
       ))}
     </div>
   );
