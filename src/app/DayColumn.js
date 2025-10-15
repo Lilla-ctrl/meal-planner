3d63a990a60acee6ebf3819a6e24a7aaa4b245cd
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-10-15 15:58:13 +0200

    MealCard created

--- src/app/DayColumn.js
+++ src/app/DayColumn.js
@@ -1,17 +1,14 @@
+import MealCard from "./MealCard";
+
 export default function DayColumn({day, handleOpen, meals}) {
   
   return (
     <div className=" w-60 p-4 rounded-2xl flex flex-col m-3 gap-2">
       <h1 className="p-3 rounded-xl text-2xl font-bold w-full text-center">{day}
         </h1>
-      <ul className="flex flex-wrap justify-center gap-2 w-full">
-        
-        <li className="bg-warm-beige border border-gray-300 p-3 w-full text-center rounded-xl">Breakfast</li>
-        <li className="bg-warm-beige border border-gray-300 p-3 w-full text-center rounded-xl">Snack *optional*</li>
-        <li className="bg-warm-beige border border-gray-300 p-3 w-full text-center rounded-xl">Lunch</li>
-        <li className="bg-warm-beige border border-gray-300 p-3 w-full text-center rounded-xl">Snack *optional*</li>
-        <li className="bg-warm-beige border border-gray-300 p-3 w-full text-center rounded-xl">Dinner</li>
-      </ul>
+      <div className="flex flex-wrap justify-center gap-2 w-full">
+          <MealCard />     
+      </div>
       <button onClick={() => handleOpen(day)} className="bg-warm-button border border-gray-300 p-3 rounded-xl">+ Add meal</button>
     </div>
   );
---
+++ src/app/MealCard.js
@@ -0,0 +1,7 @@
+export default function MealCard() {
+  return (
+    <div className="bg-warm-beige border border-gray-300 p-3 w-full text-center rounded-xl">
+      Breakfast
+    </div>
+  );
+}
